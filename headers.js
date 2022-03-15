8e5a7ec5aabc75568a305c7c2ae9ea9c210bab54
Madhav Kauntia <mkauntia@gmail.com>
2022-03-15 22:19:47 +0530

    Added security headers

---
+++ headers.js
@@ -0,0 +1,41 @@
+/**
+ * Security headers used in the app
+ * @see https://infosec.mozilla.org/guidelines/web_security
+ */
+
+const headers = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "Server",
+    value: "Apache", // phony server value
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "sameorigin",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=*", // allow specified policies here
+  },
+];
+
+export default headers;
